0535506821841d518f13327e9004e4e4a1fb564a
narvita <narkha21@gmail.com>
2022-09-06 18:51:42 +0400

    chek users

---
+++ contract1.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Casino {
+    uint256 public minVal;
+    uint256 public maxVal;
+    uint256 public Cap;
+    uint256 public payments;
+    uint256 public profit;
+    address[] public adressList;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+        minVal = 1000000000000000000;
+        maxVal = 3000000000000000000;
+        Cap = 10000000000000000000;
+        profit = 0;
+        payments = 0;
+    }  
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Access restricted, not an owner");
+        _;
+    }
+
+    function setMinTopUp(uint256  val) public onlyOwner() {
+        require(val < maxVal, 'Val should not be max then ${maxVal}');
+        minVal = val;
+    }
+
+    function setMaxTopUp(uint256  val) public onlyOwner() {
+        require( val > minVal);
+        maxVal = val;
+    }
+    
+    function setCap(uint256 val) public onlyOwner() {
+        Cap = val;
+    }
+
+    function insertAddress(address sender) public returns (bool) {
+        for(uint i = 0; i < adressList.length; i++) {
+            if (adressList[i] != sender) {
+                adressList.push(sender);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    modifier isUserAllowed(address sender) {
+        for(uint256 i = 0; i < adressList.length; i++) {
+            require(adressList[i] != sender, "Address already exist");
+            adressList.push(sender);
+        }
+        _;
+    }
+
+     function clearAddresList() private {
+        delete adressList;
+    }
+
+
+    function topUp() public payable isUserAllowed(msg.sender) {
+        require(msg.value > minVal && msg.value < maxVal, "Value is note within allowed boundaries");
+        payments += msg.value;
+        if( payments >= Cap) {
+            address payable to = payable(msg.sender);
+            to.transfer((payments/100)*80);
+            profit += (payments/100)*20;
+            payments = 0;
+            clearAddresList();
+        }
+    }
+
+
+    function withdrawAll() public onlyOwner {
+        address payable Owner = payable(owner);
+        Owner.transfer(profit);
+        profit = 0;
+    }
+}
